0ce138b6a18e951e5c3cb60866af5e96b3d94798
Ivan Almada <ivan.almada@me.com>
2024-05-07 12:37:31 -0600

    Created MVVM project structure and Config struct

---
+++ pattern-invert-macos/Model/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  pattern-invert-macos
+//
+//  Created by Iván Almada on 5/7/24.
+//
+
+import Foundation
+
+struct Config {
+    static let numberOfStimulus = 180
+    static let timeInterval = 1.0
+    static let broadcastIP1 = "192.168.1.196"
+    static let broadcastIP2 = "192.168.1.199"
+}
